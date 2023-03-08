21d105342733dd3d33ce9ae4a21d19e10a74a3f4
Guilherme Andrade <guilhermeandrade.profissional@outlook.com>
2023-03-08 13:07:35 -0300

    feat: Criação do Padrão de Arquitetura

---
+++ public/site/components/footer.php
@@ -0,0 +1,9 @@
+<footer class="container">
+    <p class="float-end">
+        <a href="#">Topo</a>
+    </p>
+
+    <p>
+        © 2023 Sócrates Imóveis. · <a href="#">Privacy</a> · <a href="#">Terms</a> · <a href="../admin/dashboard/index.php">Dashboard</a>
+    </p>
+</footer>
